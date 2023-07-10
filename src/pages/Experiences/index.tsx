349858f76522c6977c5c30d4df7d3a38c21c338d
FelipeToledo <felipetoledo.nextage@gmail.com>
2023-07-10 11:34:44 -0300

    feat: ajuste genérico

--- src/pages/Experiences/index.tsx
+++ src/pages/Experiences/index.tsx
@@ -18,8 +18,7 @@ export function Experiences() {
 >
   <TimelineContent>
     <div className="timeline-element-header">
-      <LogoProject className="project-image" src="https://img.swapcard.com/?u=https%3A%2F%2Fcdn-api.swapcard.com%2Fpublic%2Fimages%2F502839264f754501a74f6454b4b0dc7a.png&q=0.8&m=fit&w=400&h=200" alt="Coopercarga Logo" />
-      <h3 className="vertical-timeline-element-title">Coopercarga</h3>
+      <h3 className="vertical-timeline-element-title">Empresa de Transporte e Cargas</h3>
     </div>
     <p>
       Desenvolvimento e sustentação de um sistema de gestão de logística para a empresa. O projeto envolve a criação de uma plataforma web abrangente, fornecendo recursos como rastreamento de cargas, gestão de frota e controle de estoque.
@@ -35,11 +34,10 @@ export function Experiences() {
           >
               <TimelineContent>
     <div className="timeline-element-header">
-      <LogoProject className="project-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfWyd9fYM2QZKuSqlXB75EhQZIeiVEUYZRbX89zHcX1SOhIvm4unyiYgy3dbAnU-5nWOU&usqp=CAU" alt="Coopercarga Logo" />
-      <h3 className="vertical-timeline-element-title">Gestox</h3>
+      <h3 className="vertical-timeline-element-title">Empresa de Gestão de Estoque</h3>
     </div>
               <p>
-              Participei do desenvolvimento do aplicativo Gestox, um sistema de gestão de estoque para empresas. A aplicação permitia o registro de entrada e saída de produtos, controle de estoque em tempo real e geração de relatórios de desempenho na web.
+              Participei do desenvolvimento do aplicativo, um sistema de gestão de estoque para empresas. A aplicação permitia o registro de entrada e saída de produtos, controle de estoque em tempo real e geração de relatórios de desempenho na web.
               </p>
             </TimelineContent>
           </VerticalTimelineElement>
@@ -52,8 +50,7 @@ export function Experiences() {
           >
   <TimelineContent>
     <div className="timeline-element-header">
-      <LogoProject className="project-image" src="https://media.licdn.com/dms/image/C4D0BAQHTqoGG9j66wQ/company-logo_200_200/0/1571922472346?e=2147483647&v=beta&t=ZkhD9lvWxizcrfOluowGQ6a2QCxW55bbR7JEWKooz10" alt="Coopercarga Logo" />
-      <h3 className="vertical-timeline-element-title">Cessão de Créditos</h3>
+      <h3 className="vertical-timeline-element-title">Empresa de concessão de créditos</h3>
     </div>
               <p>
               Contribuí para o desenvolvimento de um sistema online para facilitar a cessão de créditos entre empresas. O projeto envolveu a criação de uma plataforma segura e eficiente, permitindo que as empresas pudessem transferir e negociar créditos de forma simplificada.
@@ -68,7 +65,7 @@ export function Experiences() {
             icon={<TimelineDot />}
           >
             <TimelineContent>
-              <h3 className="vertical-timeline-element-title">Consignado</h3>
+              <h3 className="vertical-timeline-element-title">Empresa de empréstimos consignados</h3>
               <p>
               Fui parte da equipe responsável pelo desenvolvimento de um sistema de empréstimo consignado. O projeto incluiu a criação de um portal web para solicitação de empréstimos, análise de crédito e geração de contratos.
               </p>
@@ -82,7 +79,7 @@ export function Experiences() {
             icon={<TimelineDot />}
           >
             <TimelineContent>
-              <h3 className="vertical-timeline-element-title">Meu Cobrador</h3>
+              <h3 className="vertical-timeline-element-title">Empresa de cobranças</h3>
               <p>
               Participei do projeto de gestão de cobranças para empresas, no qual desempenhei um papel na migração bem-sucedida do sistema de nuvem da AWS para a plataforma Google Cloud. Através dessa migração, otimizamos a infraestrutura e garantimos maior escalabilidade e desempenho para a aplicação. 
               </p>
