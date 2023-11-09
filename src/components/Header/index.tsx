20d262620d086dddd8c89a155709f010369da0e6
Felipe <felipetoledo88@gmail.com>
2023-11-09 11:55:08 -0300

    feat: menu hamburguer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,40 +1,47 @@
+import { useState } from 'react';
 import { AiOutlineUser, AiOutlineTool, AiOutlineMail, AiTwotoneHome, AiOutlineContacts } from "react-icons/ai";
-import { Header, Icon, ItemIcon, MenuItem, MenuList } from "./styles";
+import { Header, Icon, ItemIcon, MenuItem, MenuList, Hamburger, Sidebar } from "./styles";
 import { Link } from "react-router-dom";
 import './../../reset.css';
 
 export function HeaderPage() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <Header>
-      <a href="/">
-        <Icon />
-      </a>
-      <MenuList>
-        <MenuItem>
-          <ItemIcon>
-            <AiTwotoneHome />
-          </ItemIcon>
-          <Link to="/">HOME</Link>
-        </MenuItem>
-        <MenuItem>
-          <ItemIcon>
-            <AiOutlineUser />
-          </ItemIcon>
-          <Link to="/about-me">SOBRE</Link>
-        </MenuItem>
-        <MenuItem>
-          <ItemIcon>
-            <AiOutlineTool />
-          </ItemIcon>
-          <Link to="/experiences">EXPERIÊNCIAS</Link>
-        </MenuItem>
-        <MenuItem>
-          <ItemIcon>
-            <AiOutlineContacts />
-          </ItemIcon>
-          <Link to="/contact">CONTATO</Link>
-        </MenuItem>
-      </MenuList>
+      <Hamburger onClick={() => setIsOpen(!isOpen)} isOpen={isOpen}>
+        <div />
+        <div />
+        <div />
+      </Hamburger>
+      <Sidebar isOpen={isOpen}>
+        <MenuList>
+          <MenuItem>
+            <ItemIcon>
+              <AiTwotoneHome />
+            </ItemIcon>
+            <Link to="/">HOME</Link>
+          </MenuItem>
+          <MenuItem>
+            <ItemIcon>
+              <AiOutlineUser />
+            </ItemIcon>
+            <Link to="/about-me">SOBRE</Link>
+          </MenuItem>
+          <MenuItem>
+            <ItemIcon>
+              <AiOutlineTool />
+            </ItemIcon>
+            <Link to="/experiences">EXPERIÊNCIAS</Link>
+          </MenuItem>
+          <MenuItem>
+            <ItemIcon>
+              <AiOutlineContacts />
+            </ItemIcon>
+            <Link to="/contact">CONTATO</Link>
+          </MenuItem>
+        </MenuList>
+      </Sidebar>
     </Header>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -27,25 +27,6 @@ export const Icon = styled(FaCode)`
   }
 `;
 
-export const MenuList = styled.ul`
-  display: flex;
-  align-items: center;
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  margin-left: auto;
-
-  a { 
-    color: currentColor;
-    text-decoration: none;
-  } 
-
-  @media (max-width: 768px) {
-    display: flex;
-    flex-wrap: nowrap;
-  }
-`;
-
 export const MenuItem = styled.li`
   font-family: 'Bebas Neue', sans-serif;
   font-size: 17px;
@@ -66,3 +47,79 @@ export const MenuItem = styled.li`
     font-size: 17px;
   }
 `;
+
+export const Hamburger = styled.div`
+  display: none;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  cursor: pointer;
+  z-index: 30;
+
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background: ${props => props.isOpen ? '#fff' : '#333'};
+    border-radius: 10px;
+    transition: all 0.3s linear;
+  }
+
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+export const Sidebar = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  position: fixed;
+  transform: ${props => props.isOpen ? 'translateX(0)' : 'translateX(-100%)'};
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 80%;
+  background-color: #333;
+  transition: transform 0.3s ease-in-out;
+  padding: 10px 10px;
+  z-index: 20;
+
+  @media (min-width: 769px) {
+    position: static;
+    transform: none;
+    flex-direction: row;
+    background-color: transparent;
+    height: auto;
+    width: auto;
+    align-items: center;
+    padding-left: 0;
+  }
+`;
+
+export const MenuList = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  margin-left: auto;
+
+  a { 
+    color: #fff;
+    text-decoration: none;
+    font-size: 1.5rem;
+    width: 100%;
+    text-align: left;
+  } 
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100%;
+    gap: 35px;
+    margin-top: 70px;
+  }
+`;
+
