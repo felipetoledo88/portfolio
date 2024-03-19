3f5c955d91ce3005ef347c5f358631a4fa269ed6
Felipe Toledo <f.toledo@nextage.com.br>
2024-03-19 15:40:39 -0300

    feat: ajuste de projetos

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,25 +5,25 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 const items = [
   {
     id: 1,
-    title: "React Commerce",
-    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    title: "Transporte e Cargas",
+    img: "https://media.istockphoto.com/id/859916128/pt/foto/truck-driving-on-the-asphalt-road-in-rural-landscape-at-sunset-with-dark-clouds.jpg?s=612x612&w=0&k=20&c=DIIIpexvcPtYWRwEXSS7RjbAglC4B5k_0XdW0b6Cz-k=",
+    desc: "Aprimoramento e manutenção de um sistema abrangente de gestão logística. Este projeto abrangia a concepção e implementação de uma plataforma Web utilizando tecnologias React e NodeJS, bem como o desenvolvimento de uma aplicação móvel em React Native. A arquitetura incluía integração de Microserviços, configuração de Pipelines, e implementação de APIs. O propósito central era oferecer funcionalidades como rastreamento de cargas, gestão de frotas e controle de estoque, visando otimizar operações logísticas de forma eficiente.",
   },
   {
     id: 2,
-    title: "Next.js Blog",
+    title: "Mobile",
     img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
   },
   {
     id: 3,
-    title: "Vanilla JS App",
+    title: "Smartwatch",
     img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
   },
   {
     id: 4,
-    title: "Music App",
+    title: "I.A",
     img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
   },
@@ -43,12 +43,11 @@ const Single = ({ item }) => {
       <div className="container">
         <div className="wrapper">
           <div className="imageContainer" ref={ref}>
-            <img src={item.img} alt="" />
+            <img className="image" src={item.img} alt="" />
           </div>
           <motion.div className="textContainer" style={{y}}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <button>See Demo</button>
           </motion.div>
         </div>
       </div>
@@ -72,7 +71,7 @@ const Portfolio = () => {
   return (
     <div className="portfolio" ref={ref}>
       <div className="progress">
-        <h1>Projetos Realizados</h1>
+        <h1>Projetos e Experiências</h1>
         <motion.div style={{ scaleX }} className="progressBar"></motion.div>
       </div>
       {items.map((item) => (
